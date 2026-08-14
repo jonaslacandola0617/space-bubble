7dba1f524f6ab8588fce0ab879f1311c254fdb17
Jonas Lacandola <jonaslacandola07@gmail.com>
2026-08-14 12:43:18 +0800

    fix: include space members in realtime publication

--- supabase/realtime.sql
+++ supabase/realtime.sql
@@ -1,2 +1,3 @@
 alter publication supabase_realtime add table public.bubbles;
 alter publication supabase_realtime add table public.checkins;
+alter publication supabase_realtime add table public.space_members;
